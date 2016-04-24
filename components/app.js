13608a9d1ed7433be646d09314a6b83546bafbaa
Jun Zheng <arthurjordan@gmail.com>
2016-04-24 20:17:40 +1000

    added store, store.subscribe, store.getState(), store.dispatch

--- components/app.js
+++ components/app.js
@@ -1,14 +1,35 @@
-function counter(state, action){
-  if(typeof state === 'undefined'){
-    return 0
-  }
-  else if(action.type == 'INCREMENT'){
-    return state + 1
-  }
-  else if(action.type == 'DECREMENT'){
-    return state - 1
-  }
-  else {
-      return state
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+      break;
+    case 'DECREMENT':
+      return state - 1;
+      break;
+    default:
+      return state;
   }
 }
+
+// const { createStore } = Redux;
+// var createStore = Redux.createStore;
+import { createStore } from 'redux';
+//holde current state, let you dispatch action,
+const store = createStore(counter); // pass in the reducer
+
+console.log(store.getState());
+store.dispatch({type: 'INCREMENT'});
+console.log(store.getState());
+
+const render = () => {
+  document.body.innerText = store.getState();
+  console.log(store.getState());
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({type: 'INCREMENT'})
+})
