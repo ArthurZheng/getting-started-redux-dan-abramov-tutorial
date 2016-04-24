5c723a75d85e7a104a9e22b3e527505af0ae1777
Jun Zheng <arthurjordan@gmail.com>
2016-04-24 20:26:52 +1000

    manually reimplement createStore function

--- components/app.js
+++ components/app.js
@@ -14,8 +14,31 @@ const counter = (state = 0, action) => {
 
 // const { createStore } = Redux;
 // var createStore = Redux.createStore;
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 //holde current state, let you dispatch action,
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener() );
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter( l => l !== listener);
+    };
+  };
+  dispatch({});
+  return { getState, dispatch, subscribe};
+};
+
+
 const store = createStore(counter); // pass in the reducer
 
 console.log(store.getState());
