0d346bb5e95990605b16d1065d89c2b30cd6b595
Jun Zheng <arthurjordan@gmail.com>
2016-04-25 09:29:38 +1000

    added more reducers, set_visibility_filter reducer, reducer composition

--- components/app.js
+++ components/app.js
@@ -76,7 +76,7 @@ const addCounter = (list) => {
   return [...list, 0];
 }
 
-const removeCounter = (list, index) {
+const removeCounter = (list, index) => {
   // return list.slice(0, index).concat(list.slice(index + 1));
   // return [
   //   ...list.slice(0, index),
@@ -117,13 +117,13 @@ const todo = (state, action) => {
     case 'TOGGLE_TODO':
       if(state.id !== action.id) {
         return state;
+      } else {
+        return Object.assign({}, state, {
+          completed: !state.completed
+        });
       }
-      return {
-        ...state,
-        completed: !state.completed
-      };
-      default:
-        return state;
+    default:
+      return state;
     }
 };
 
@@ -143,12 +143,86 @@ const todos = (state = [], action) => {
   }
 };
 
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+      break;
+    default:
+      return state;
+  }
+};
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
+  };
+};
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
 const store = createStore(counter_reducer);
+// const store = createStore(todos);
+// const store = createStore(todoApp);
+
+console.log('Initial State:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching ADD_TODO');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+
+console.log('current state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching ADD_TODO');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go Shopping'
+});
+
+console.log('current state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching TOGGLE_TODO');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0,
+});
+console.log('current state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
+});
+
+console.log('current state:');
+console.log(store.getState());
+console.log('----------------');
+
 
 const render = () => {
   ReactDOM.render(
